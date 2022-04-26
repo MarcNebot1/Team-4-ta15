7a62abd181691fa2b453b60ca2c71255f00e4e3f
MarcPadrell <79224406+MarcPadrell@users.noreply.github.com>
2022-04-26 12:04:10 +0200

    Add files via upload

---
+++ comandes_sql_ex1_ta15.sql
@@ -0,0 +1,20 @@
+use los_directores;
+
+/*Insertem valors als despatxos*/
+insert into despachos values (1,20), (2,25), (3,50), (4,56), (5,68),(6,78),(7,41),(8,30),(9,61),(10,100);
+
+/*Insertem valors de directors*/
+insert into directores values ('12345678', 'Jose Marin', '41526374', 1);
+insert into directores values ('32165498', 'Cristobal', '33336699', 6);
+insert into directores values ('33336699', 'Luis', '78945612', 9);
+insert into directores values ('41526374', 'Ester', '41526374', 2);
+insert into directores values ('55554488', 'Tomas', '55554488', 8);
+insert into directores values ('64531298', 'Maria', '64531298', 10);
+insert into directores values ('74859641', 'David', '12345678', 3);
+insert into directores values ('74896453', 'Mar', '12345678', 10);
+insert into directores values ('75395185', 'Leo', '74859641', 7);
+insert into directores values ('78945612', 'Polo', '78945612', 5);
+insert into directores values ('85528552', 'Ariadna', '55554488', 4);
+insert into directores values ('88888878', 'Manel', '33336699', 5);
+
+
